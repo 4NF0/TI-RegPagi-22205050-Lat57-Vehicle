c57a16b2aec7327c9803f52098320d6eff3ed3e4
4NF0 <148747658+4NF0@users.noreply.github.com>
2023-11-30 11:35:07 +0700

    commit

---
+++ Latihan57.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan57;
+
+/**
+ *
+ * @author nm
+ * Nama : Adi Naufal F
+ * Nim : 22205050
+ * Kelas/Semester : TI/3
+ * Deskripsi Program : Program ini berisi tentang vehicle berupa bicycle dan skateboard.
+ */
+public class Latihan57 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Bicycle bcl = new Bicycle();
+        bcl.setBrand("Trek Bike");
+        bcl.setModel("7.4FX");
+        bcl.setGearCount(23);
+        System.out.println("Brand : "+bcl.getBrand());
+        System.out.println("Model : "+bcl.getModel());
+        System.out.println("Jumlah Gear : "+bcl.getGearCount());
+        System.out.println("");
+        
+        Skateboard sktb = new Skateboard();
+        sktb.setBrand("Ally Skate");
+        sktb.setModel("Rocket");
+        sktb.setBoardLeght(54.5);
+        System.out.println("Brand : "+sktb.getBrand());
+        System.out.println("Model : "+sktb.getModel());
+        System.out.println("Panjangnya Board : "+sktb.getBoardLeght());
+    }
+    
+}
---
+++ Skateboard.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan57;
+
+/**
+ *
+ * @author nm
+ */
+public class Skateboard extends Vehicle {
+    private double myBoardLeght;
+    public Skateboard(){
+        System.out.println("Skateboard");
+    }
+
+    public double getBoardLeght() {
+        return myBoardLeght;
+    }
+
+    public void setBoardLeght(double myBoardLeght) {
+        this.myBoardLeght = myBoardLeght;
+    }  
+}
